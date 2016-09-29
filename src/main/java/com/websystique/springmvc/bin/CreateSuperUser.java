cc846ed957d4f49d2d442c28a8d912a76a6545c5
Arka Dutta <arkadutta@Arkas-MacBook-Air.local>
2016-09-30 02:28:19 +0530

    1 functionality

--- src/main/java/com/websystique/springmvc/bin/CreateSuperUser.java
+++ src/main/java/com/websystique/springmvc/bin/CreateSuperUser.java
@@ -18,24 +18,24 @@ import java.util.regex.Pattern;
  */
 public class CreateSuperUser {
 
-    private Map<String,String> keyBoardInput ;
+    private Map<String, String> keyBoardInput;
     private static SessionFactory sessionFactory = null;
     private static Session session = null;
     private static Transaction tx = null;
 
-    public CreateSuperUser(){
-        keyBoardInput = new HashMap<String,String>();
+    public CreateSuperUser() {
+        keyBoardInput = new HashMap<String, String>();
     }
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         CreateSuperUser user = new CreateSuperUser();
         user.getInputFromUser();
-        try{
+        try {
             user.createUser();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             HibernateAnnotationUtil.shutDown();
         }
 
@@ -43,14 +43,7 @@ public class CreateSuperUser {
         System.out.println("The user have been created . Now try login to the system");
 
 
-
-
-
-
-
-
     }
-
 
 
     private void createUser() {
@@ -73,7 +66,6 @@ public class CreateSuperUser {
         user.setPassword(pwd);
 
 
-
         user.setIsactive(true);
         user.setIsotp(true);
         user.setIsadmin(true);
@@ -85,46 +77,14 @@ public class CreateSuperUser {
 
         tx.commit();
         HibernateAnnotationUtil.shutDown();
-
-
-
-        /*String hql = "from CategoryLearnedData Cat where Cat.type=:typeStr";
-        System.out.println(hql);
-        Query query = session.createQuery(hql);
-        query.setParameter("typeStr", type);
-
-        query.setReadOnly(true);
-        query.setFetchSize(dbFetchSize);
-        //query.setParameter(":password", login.getPassword());
-        ScrollableResults results = query.scroll(ScrollMode.FORWARD_ONLY);
-        int count = 0;
-        while (results.next()) {
-            if (++count > 0 && count % 100 == 0) {
-                System.out.println("Fetched " + count + " entities");
-            }
-            CategoryLearnedData demoEntity = (CategoryLearnedData) results.get()[0];
-            //Process and write result
-            dbLearnedJson.add(demoEntity.getLearnedData());
-            session.evict(demoEntity);
-            demoEntity = null;
-        }
-        results.close();
-
-        //Commit transaction
-        tx.commit();
-        System.out.println(dbLearnedJson.size());
-        */
-
-
-
     }
 
-    private boolean verifyIfFieldQualifies(String value , InputEnum fieldName){
+    private boolean verifyIfFieldQualifies(String value, InputEnum fieldName) {
 
-        if(fieldName == null)
+        if (fieldName == null)
             return false;
-        else{
-            switch (fieldName){
+        else {
+            switch (fieldName) {
                 case USERNAME:
                     String EMAIL_PATTERN =
                             "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
@@ -140,13 +100,13 @@ public class CreateSuperUser {
                     return value.matches("[a-zA-Z]+");
 
                 case MOBILE:
-                    if(value.trim().isEmpty())
+                    if (value.trim().isEmpty())
                         return true;
                     else
                         return value.trim().matches("[0-9]+");
 
                 case PASSWORD:
-                    if(value.length() < 8)
+                    if (value.length() < 8)
                         return false;
                     else
                         return true;
@@ -158,7 +118,7 @@ public class CreateSuperUser {
         }
     }
 
-    private void getInputFromUser(){
+    private void getInputFromUser() {
 
         Scanner keyboard = new Scanner(System.in);
         System.out.println("This utlity is used to create a super-user.\nPlease follow along to create a user who has admin priviledge");
@@ -171,17 +131,17 @@ public class CreateSuperUser {
         String inpVal = keyboard.nextLine().trim();
 
         flag = verifyIfFieldQualifies(inpVal, InputEnum.USERNAME);
-        if(flag)
-            keyBoardInput.put(InputEnum.USERNAME.value,inpVal);
-        else{
-            while(!flag){
-                System.out.println("The username : "+inpVal + " provided is not a valid email-id . Please Enter a valid email id - " );
+        if (flag)
+            keyBoardInput.put(InputEnum.USERNAME.value, inpVal);
+        else {
+            while (!flag) {
+                System.out.println("The username : " + inpVal + " provided is not a valid email-id . Please Enter a valid email id - ");
                 inpVal = keyboard.nextLine();
 
                 flag = verifyIfFieldQualifies(inpVal, InputEnum.USERNAME);
 
             }
-            keyBoardInput.put(InputEnum.USERNAME.value,inpVal);
+            keyBoardInput.put(InputEnum.USERNAME.value, inpVal);
         }
 
         //enter firstname
@@ -189,18 +149,18 @@ public class CreateSuperUser {
         inpVal = keyboard.nextLine().trim();
 
         flag = verifyIfFieldQualifies(inpVal, InputEnum.FIRST_NAME);
-        if(flag)
-            keyBoardInput.put(InputEnum.FIRST_NAME.value,inpVal);
-        else{
-            while(!flag){
-                System.out.println("The First Name : "+inpVal + " provided is not a valid name . Please Enter a valid name with only" +
-                        " english letters - " );
+        if (flag)
+            keyBoardInput.put(InputEnum.FIRST_NAME.value, inpVal);
+        else {
+            while (!flag) {
+                System.out.println("The First Name : " + inpVal + " provided is not a valid name . Please Enter a valid name with only" +
+                        " english letters - ");
                 inpVal = keyboard.nextLine();
 
                 flag = verifyIfFieldQualifies(inpVal, InputEnum.FIRST_NAME);
 
             }
-            keyBoardInput.put(InputEnum.FIRST_NAME.value,inpVal);
+            keyBoardInput.put(InputEnum.FIRST_NAME.value, inpVal);
         }
 
         //enter last name
@@ -208,18 +168,18 @@ public class CreateSuperUser {
         inpVal = keyboard.nextLine().trim();
 
         flag = verifyIfFieldQualifies(inpVal, InputEnum.LAST_NAME);
-        if(flag)
-            keyBoardInput.put(InputEnum.LAST_NAME.value,inpVal);
-        else{
-            while(!flag){
-                System.out.println("The Last Name : "+inpVal + " provided is not a valid name . Please Enter a valid name with only" +
-                        " english letters - " );
+        if (flag)
+            keyBoardInput.put(InputEnum.LAST_NAME.value, inpVal);
+        else {
+            while (!flag) {
+                System.out.println("The Last Name : " + inpVal + " provided is not a valid name . Please Enter a valid name with only" +
+                        " english letters - ");
                 inpVal = keyboard.nextLine();
 
                 flag = verifyIfFieldQualifies(inpVal, InputEnum.LAST_NAME);
 
             }
-            keyBoardInput.put(InputEnum.LAST_NAME.value,inpVal);
+            keyBoardInput.put(InputEnum.LAST_NAME.value, inpVal);
         }
 
         //enter mobile
@@ -227,19 +187,19 @@ public class CreateSuperUser {
         inpVal = keyboard.nextLine().trim();
 
         flag = verifyIfFieldQualifies(inpVal, InputEnum.MOBILE);
-        if(flag)
-            keyBoardInput.put(InputEnum.MOBILE.value,inpVal);
-        else{
-            while(!flag){
-                System.out.println("The Mobile Number : "+inpVal + " provided is not a valid name . Please either keep empty or " +
+        if (flag)
+            keyBoardInput.put(InputEnum.MOBILE.value, inpVal);
+        else {
+            while (!flag) {
+                System.out.println("The Mobile Number : " + inpVal + " provided is not a valid name . Please either keep empty or " +
                         "enter a valid number with only" +
-                        " numeric characters - " );
+                        " numeric characters - ");
                 inpVal = keyboard.nextLine();
 
                 flag = verifyIfFieldQualifies(inpVal, InputEnum.MOBILE);
 
             }
-            keyBoardInput.put(InputEnum.MOBILE.value,inpVal);
+            keyBoardInput.put(InputEnum.MOBILE.value, inpVal);
         }
 
         //reading password
@@ -254,7 +214,7 @@ public class CreateSuperUser {
         readPassword(console);
 
 
-        while(!keyBoardInput.get(InputEnum.PASSWORD.value).equals(keyBoardInput.get(InputEnum.RETYPE_PASSWORD.value))){
+        while (!keyBoardInput.get(InputEnum.PASSWORD.value).equals(keyBoardInput.get(InputEnum.RETYPE_PASSWORD.value))) {
             System.out.println("Password didnot match  ---------- ");
             readPassword(console);
         }
@@ -262,15 +222,15 @@ public class CreateSuperUser {
         System.out.println("Details enetered - ");
         Set<String> keys = keyBoardInput.keySet();
 
-        for(String aKey : keys){
-            System.out.println(aKey + " : "+ keyBoardInput.get(aKey));
+        for (String aKey : keys) {
+            System.out.println(aKey + " : " + keyBoardInput.get(aKey));
         }
 
         System.out.println("Thanks for Entering the details. Now wait for the user to be created in database");
 
     }
 
-    private void readPassword(Console console){
+    private void readPassword(Console console) {
         System.out.print("Enter your password: Should be atleast 8 character long ");
         char[] password = console.readPassword();
 
@@ -278,10 +238,10 @@ public class CreateSuperUser {
         boolean flag = false;
 
         flag = verifyIfFieldQualifies(inpVal, InputEnum.PASSWORD);
-        if(flag)
-            keyBoardInput.put(InputEnum.PASSWORD.value,inpVal);
-        else{
-            while(!flag){
+        if (flag)
+            keyBoardInput.put(InputEnum.PASSWORD.value, inpVal);
+        else {
+            while (!flag) {
                 System.out.println("The password provided is not a valid password . " +
                         "Please enter a password which is atleast 8 characters long ");
                 password = console.readPassword();
@@ -290,7 +250,7 @@ public class CreateSuperUser {
                 flag = verifyIfFieldQualifies(inpVal, InputEnum.PASSWORD);
 
             }
-            keyBoardInput.put(InputEnum.PASSWORD.value,inpVal);
+            keyBoardInput.put(InputEnum.PASSWORD.value, inpVal);
         }
 
         //reading retype-password option
@@ -298,7 +258,7 @@ public class CreateSuperUser {
         password = console.readPassword();
 
         inpVal = new String(password);
-        keyBoardInput.put(InputEnum.RETYPE_PASSWORD.value,inpVal);
+        keyBoardInput.put(InputEnum.RETYPE_PASSWORD.value, inpVal);
 
 
     }
